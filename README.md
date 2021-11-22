e31253cde8b7f6c1d478c250165ad1212a52a8a5
Berik Visschers <berik@visschers.nu>
2021-11-23 00:12:26 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AnyFailableSequence
+
+A description of this package.
---
+++ Sources/AnyFailableSequence/AnyFailableSequence.swift
@@ -0,0 +1,6 @@
+public struct AnyFailableSequence {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
